d4fc66af64202303c0d33d259c84f035de615b2e
Adelgrin <lamaral2002@gmail.com>
2026-01-08 09:05:23 -0300

    i3-status-rust added

--- dot_config/yazi/init.lua
+++ dot_config/yazi/init.lua
@@ -1,4 +1,4 @@
-require("git"):setup()
+-- require("git"):setup()
 require("relative-motions"):setup({ show_numbers = "relative", show_motion = true, enter_mode = "first" })
 require("telegram-send"):setup({
 	command = "telegram-send --file",
